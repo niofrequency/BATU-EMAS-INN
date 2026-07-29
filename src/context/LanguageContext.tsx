a78a1748cc74fdd5e5d85f0e7851957a0431d770
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-07-29 13:49:39 +0900

    Create LanguageContext.tsx

---
+++ src/context/LanguageContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState } from 'react';
+import { translations, Language } from '../data/translations';
+
+interface LanguageContextType {
+  lang: Language;
+  setLang: (lang: Language) => void;
+  t: typeof translations['en'];
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+
+export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [lang, setLang] = useState<Language>('en');
+  const t = translations[lang];
+
+  return (
+    <LanguageContext.Provider value={{ lang, setLang, t }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+  return context;
+};
